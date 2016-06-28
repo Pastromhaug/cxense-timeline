c209d237246c6eadb483046fcb665eb324cfb689
Per Andre Stromhaug <pas282@cornell.edu>
2016-06-28 12:51:25 +0200

    may have fixed colors go inlude 'Closed'

--- src/components/presentation/cards.js
+++ src/components/presentation/cards.js
@@ -33,27 +33,28 @@ class  Cards extends React.Component {
     }
 
     static getColors(issue) {
+        var status1 = ['Resolved', 'Closed'];
         var group1 = ['None', 'Not started' + 'Functional spec in progress'];
         var group2 = ['Functional spec done', 'Dev planning in progress']
-        if (issue.status === 'Resolved' && issue.resolution === 'Fixed') {
+        if (status1.indexOf(issue.status) != -1 && issue.resolution === 'Fixed') {
             return COLORS.a;
         }
-        else if (issue.status === 'Resolved' && issue.resolution === "Won't Fix") {
+        else if (status1.indexOf(issue.status) != -1 && issue.resolution === "Won't Fix") {
             return COLORS.b;
         }
-        else if (issue.status !== 'Resolved' &&
+        else if (status1.indexOf(issue.status) == -1 &&
             (group1.indexOf(issue.resolution) != -1 || group1.indexOf(issue.resolution2) != -1)) {
             return COLORS.c;
         }
-        else if (issue.status !== 'Resolved' &&
+        else if (status1.indexOf(issue.status) == -1 &&
             (group2.indexOf(issue.resolution) != -1 || group2.indexOf(issue.resolution2) != -1)) {
             return COLORS.d;
         }
-        else if (issue.status !== 'Resolved' &&
+        else if (status1.indexOf(issue.status) == -1 &&
             (issue.resolution === 'Ready for coding' || issue.resolution2 === 'Ready for coding')) {
             return COLORS.d;
         }
-        else if (issue.status !== 'Resolved' &&
+        else if (status1.indexOf(issue.status) == -1 &&
             (issue.resolution === 'N/A' || issue.resolution2 === 'N/A')) {
             return COLORS.e;
         }
