6021dc620d99285c92501cdaca45976427a25274
Per Andre Stromhaug <pas282@cornell.edu>
2016-06-30 12:03:13 +0200

    changed get from '/' to '/*' to allow linking of queries

--- src/prod-server.js
+++ src/prod-server.js
@@ -26,7 +26,7 @@ app.get('/test', function (req, res) {
         })
 });
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.resolve(__dirname + '/../prod/prod-index.html'));
 });
 
