fe5c607d0ef28f0aec341a02bee1e9951d96011e
Per Andre Stromhaug <pas282@cornell.edu>
2016-06-30 12:08:43 +0200

    changed get from '/' to '/*' to allow linking of queries

--- src/prod-server.js
+++ src/prod-server.js
@@ -26,13 +26,15 @@ app.get('/test', function (req, res) {
         })
 });
 
+app.get('/timeline*', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../prod/prod-index.html'));
+});
+
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname + '/../prod/prod-index.html'));
 });
 
-app.get('/timeline/*', function (req, res) {
-    res.sendFile(path.resolve(__dirname + '/../prod/prod-index.html'));
-});
+
 
 
 
