680a7c3907b573eb97d26f7e37bc22e7812acec9
Per Andre Stromhaug <pas282@cornell.edu>
2016-08-05 10:49:27 +0200

    refactored drawer into own component

--- src/components/logic/visibleAppContent.js
+++ src/components/logic/visibleAppContent.js
@@ -5,65 +5,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AppContent from '../presentation/appContent';
-import {actionOpenQueryDialog, actionCloseQueryDialog,
-    actionSetSavedQueries, actionSetEditJson,
-    actionSetEditMode, actionSetQueryName} from '../../actions/queryDialog';
-import {actionTempQuery, actionApplyQueryCustom} from '../../actions/query';
-import {actionApplyColumns, actionAddColumnsCustom} from '../../actions/columns';
-import {actionLoadingStart,actionLoadingStop} from '../../actions/loading';
 
 const mapStateToProps = (state) => {
     return {
-        query: state.query.query.query,
-        queryDialog: state.queryDialog.open,
-        saved_queries: state.queryDialog.saved_queries
-    }
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        dispatchOpenQueryDialog: () => {
-            dispatch(actionOpenQueryDialog())
-        },
-        dispatchCloseQueryDialog: () => {
-            dispatch(actionCloseQueryDialog())
-        },
-        dispatchTempQuery(temp_query) {
-            dispatch(actionTempQuery(temp_query))
-        },
-        dispatchSetSavedQueries(saved_queries) {
-            dispatch(actionSetSavedQueries(saved_queries))
-        },
-        dispatchApplyQueryCustom(query) {
-            dispatch(actionApplyQueryCustom(query))
-        },
-        dispatchSetEditJson(edit_json) {
-            dispatch(actionSetEditJson(edit_json))
-        },
-        dispatchSetEditMode(to_edit) {
-            dispatch(actionSetEditMode(to_edit))
-        },
-        dispatchSetQueryName(name) {
-            dispatch(actionSetQueryName(name))
-        },
-        dispatchApplyColumns: () => {
-            dispatch(actionApplyColumns())
-        },
-        dispatchAddColumnsCustom(columns_list) {
-            dispatch(actionAddColumnsCustom(columns_list))
-        },
-        dispatchLoadingStart() {
-            dispatch(actionLoadingStart())
-        },
-        dispatchLoadingStop() {
-            dispatch(actionLoadingStop())
-        }
+        query: state.query.query.query
     }
 };
 
 const VisibleAppContent = connect (
-    mapStateToProps,
-    mapDispatchToProps
+    mapStateToProps
 )(AppContent);
 
 export default VisibleAppContent;
---
+++ src/components/logic/visibleAppDrawer.js
@@ -0,0 +1,66 @@
+/**
+ * Created by perandre on 05.08.16.
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import AppDrawer from '../presentation/appDrawer';
+import {actionOpenQueryDialog,
+    actionSetSavedQueries, actionSetEditJson,
+    actionSetEditMode, actionSetQueryName} from '../../actions/queryDialog';
+import {actionTempQuery, actionApplyQueryCustom} from '../../actions/query';
+import {actionApplyColumns, actionAddColumnsCustom} from '../../actions/columns';
+import {actionLoadingStart,actionLoadingStop} from '../../actions/loading';
+
+const mapStateToProps = (state) => {
+    return {
+        query: state.query.query.query,
+        queryDialog: state.queryDialog.open,
+        saved_queries: state.queryDialog.saved_queries
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        dispatchOpenQueryDialog: () => {
+            dispatch(actionOpenQueryDialog())
+        },
+        dispatchTempQuery(temp_query) {
+            dispatch(actionTempQuery(temp_query))
+        },
+        dispatchSetSavedQueries(saved_queries) {
+            dispatch(actionSetSavedQueries(saved_queries))
+        },
+        dispatchApplyQueryCustom(query) {
+            dispatch(actionApplyQueryCustom(query))
+        },
+        dispatchSetEditJson(edit_json) {
+            dispatch(actionSetEditJson(edit_json))
+        },
+        dispatchSetEditMode(to_edit) {
+            dispatch(actionSetEditMode(to_edit))
+        },
+        dispatchSetQueryName(name) {
+            dispatch(actionSetQueryName(name))
+        },
+        dispatchApplyColumns: () => {
+            dispatch(actionApplyColumns())
+        },
+        dispatchAddColumnsCustom(columns_list) {
+            dispatch(actionAddColumnsCustom(columns_list))
+        },
+        dispatchLoadingStart() {
+            dispatch(actionLoadingStart())
+        },
+        dispatchLoadingStop() {
+            dispatch(actionLoadingStop())
+        }
+    }
+};
+
+const VisibleAppDrawer = connect (
+    mapStateToProps,
+    mapDispatchToProps
+)(AppDrawer);
+
+export default VisibleAppDrawer;
